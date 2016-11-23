4042912fe0042d99e3584b40305714e2b5ad3b7c
Jimmy Kasprzak <jimmykasprzak@gmail.com>
2016-11-23 17:14:53 +0100

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+})
+export class AppComponent implements OnInit {
+    title = 'Tour of Heroes';
+
+    constructor () { }
+
+    ngOnInit () { }
+}
