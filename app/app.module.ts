9f844f70bdca195d9e49104194d81a95c109141f
Jimmy Kasprzak <jimmykasprzak@gmail.com>
2016-11-24 16:54:54 +0100

    Use Observable instead of Promise and add a hero search.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,5 @@
+import './rxjs-extensions';
+
 import { NgModule }             from '@angular/core';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
@@ -7,6 +9,7 @@ import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent }  from './hero-search.component';
 
 import { HeroService }          from './hero.service';
 
@@ -25,7 +28,13 @@ import { InMemoryDataService } from './in-memory-data.service';
         InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
-    declarations: [ AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent ],
+    declarations: [ 
+        AppComponent, 
+        DashboardComponent, 
+        HeroDetailComponent,
+        HeroesComponent,
+        HeroSearchComponent 
+    ],
     providers: [ HeroService ],
     bootstrap: [ AppComponent ]
 })
